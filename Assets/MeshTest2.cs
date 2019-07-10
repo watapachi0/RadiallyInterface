7b644109e632ee0b0e6c06c2d9146acc5ad518ac
BzLOVEman <segareyarou@yahoo.co.jp>
2019-07-10 16:40:46 +0900

    更に色々助言を足してみた

--- Assets/MeshTest2.cs
+++ Assets/MeshTest2.cs
@@ -43,23 +43,23 @@ public class MeshTest2 : MonoBehaviour {
         new Vector3(1f,0f,-1f),
     };
     //面情報
-    int[] face = new int[] { 0, 1, 3,
-                             1, 2, 3,
+    int[] face = new int[] { 0, 3, 1,
+                             1, 3, 2,
 
-                             4, 6, 5,
-                             4, 7, 6,
+                             4, 5, 6,
+                             4, 6, 7,
 
-                             1+8, 6+8, 2+8,
-                             1+8, 5+8, 6+8,
+                             1+8, 2+8, 6+8,
+                             1+8, 6+8, 5+8,
 
-                             3+8, 2+16, 7+8,
-                             2+16, 6+16, 7+8,
+                             3+8, 7+8, 2+16,
+                             2+16, 7+8, 6+16,
 
-                             1+16, 0+8, 5+16,
-                             0+8, 4+8, 5+16,
+                             1+16, 5+16, 0+8,
+                             0+8, 5+16, 4+8,
 
-                             0+16, 7+16, 4+16,
-                             0+16, 3+16, 7+16
+                             0+16, 4+16, 7+16,
+                             0+16, 7+16, 3+16,
     };
     //UV情報
     Vector2[] uvs = new Vector2[] {
@@ -117,7 +117,7 @@ public class MeshTest2 : MonoBehaviour {
         //レンダラー追加
         //obj.AddComponent<MeshRenderer>();
         //メッシュアタッチ　　→Mesh.Colorの後の処理との意見
-        //mesh_filter.mesh = mesh;
+        mesh_filter.mesh = mesh;
 
         //色決定
         //obj.GetComponent<MeshRenderer>().material.color = new Color(1f, 0.5f, 0.5f, 1f);
@@ -125,25 +125,25 @@ public class MeshTest2 : MonoBehaviour {
         //mesh.colors = new Color(0.5f, 0.5f, 0.5f, 1f);
         //色決定3
         int tmp = mesh_filter.mesh.vertices.Length;
-        Debug.Log(tmp);
+        //Debug.Log(tmp);
         Color[] x = new Color[tmp];
         x[0] = Color.red;
         x[1] = Color.blue;
         x[2] = Color.yellow;
-        mesh_filter.mesh.colors = x;
+        //mesh_filter.mesh.colors = x;
         //色決定4
-        //obj.GetComponent<MeshRenderer>().material = _material;
+        obj.GetComponent<MeshRenderer>().material = _material;
 
 
         //メッシュアタッチ　　→Mesh.Colorの後に書いてみた
-        mesh_filter.mesh = mesh;
+        //mesh_filter.mesh = mesh;
 
         //UV情報追加
         mesh_filter.mesh.uv = uvs;
         //Boundsの再計算　　　→いらない疑惑
-        //mesh.RecalculateBounds();
+        mesh_filter.mesh.RecalculateBounds();
         //NormalMapの再計算　 →いらない疑惑
-        //mesh.RecalculateNormals();
+        mesh_filter.mesh.RecalculateNormals();
     }
 
 
