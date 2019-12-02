656998b2298668955cbbdd321b76ef5754d7cd10
BzLOVEman <segareyarou@yahoo.co.jp>
2019-12-02 10:32:25 +0900

    ログ排出
    排出個所を整理。
    他のデバッグログを消した。（実験データ撮影のため）
    また、細かい時間の表記を修正

--- Assets/Script/logSave.cs
+++ Assets/Script/logSave.cs
@@ -9,6 +9,7 @@ public class logSave : MonoBehaviour {
     private StreamWriter sw;
     private string filePath;
     private float deltaTime;
+    private float deltaTimeF;
 
     void Start() {
         string nowdate = Nowdate();
@@ -29,53 +30,74 @@ public class logSave : MonoBehaviour {
 
         /* プラットホーム依存コンパイル */
 # if UNITY_EDITOR
-        filePath += "/log" + nowdate + " " + systemName + ".txt";
+        filePath += "/log" + nowdate + " " + systemName + ".csv";
 # elif UNITY_STANDALONE_WIN
-        filePath += "/log" + nowdate + " " + systemName + ".txt";
+        filePath += "/log" + nowdate + " " + systemName + ".csv";
 # elif UNITY_ANDROID
-        filePath = Application.persistentDataPath + "/log" + " " + systemName + ".txt";
+        filePath = Application.persistentDataPath + "/log" + " " + systemName + ".csv";
 # endif
         /* プラットホーム依存コンパイル ここまで*/
 
-        sw = new StreamWriter(filePath, true);
+        sw = new StreamWriter(filePath, true, System.Text.Encoding.GetEncoding("utf-8"));
+        //nowdate = nowdate.Replace("／", "/");
+        //nowdate = nowdate.Replace("：", ":");
         sw.WriteLine(nowdate);
-        sw.Write("\tRealtime\t\tGameTime\t\t\t\tEvent");
+        string swStr = "\tRealtime\tGameTime\t\t\t\tEvent\n";
+
+        sw.Write(swStr.Replace("\t", ","));
         sw.Flush();
         sw.Close();
+        Debug.Log(swStr.Replace("\t", " "));
         deltaTime = NowTimeNum();
+        deltaTimeF = Time.fixedTime;
     }
 
-    public void LogSaving(string log) {
+    public void LogSaving(string log, bool needDeltaCulc) {
+        float ftime = Time.fixedTime;
         sw = new StreamWriter(filePath, true);
-        sw.Write("\t" + NowTime() + "\t\t");
-        sw.Write(Time.fixedTime.ToString() + "\t");
-        sw.Write("DeltaTime\t" + ( NowTimeNum() - deltaTime ).ToString("N2") + "\t");
-        deltaTime = NowTimeNum();
-        sw.WriteLine(log);
+        string swStr = "";
+        swStr += "\t" + NowTime()/*.Replace("：", ":")*/ + "\t";
+        //swStr += Time.fixedTime.ToString() + "\t";
+        if (needDeltaCulc) {
+            swStr += "DeltaTime\t" + ( NowTimeNum() - deltaTime ).ToString("N2") + "\t"+1.234f.ToString()+"\t";
+            swStr += "FixedTime\t" + ( ftime - deltaTimeF ).ToString("N2") + "\t";
+        } else {
+            swStr += "DeltaTime\t" + "none" + "\t";
+            swStr += "FixedTime\t" + "none" + "\t";
+        }
+        swStr += log + "\n";
+        if (needDeltaCulc) {
+            deltaTime = NowTimeNum();
+            deltaTimeF = ftime;
+        }
+
+        sw.Write(swStr.Replace("\t", ","));
         sw.Flush();
         sw.Close();
+        Debug.Log(swStr.Replace("\t", " "));
     }
 
     private string NowTime() {
-        string dateTimeStr = System.DateTime.Now.Hour  .ToString()  + "-"
-                           + System.DateTime.Now.Minute.ToString()  + "-"
-                           + System.DateTime.Now.Second.ToString();
+        string dateTimeStr = System.DateTime.Now.Hour.ToString("D2") + "-"
+                           + System.DateTime.Now.Minute.ToString("D2") + "-"
+                           + System.DateTime.Now.Second.ToString("D2") + "."
+                           + System.DateTime.Now.Millisecond.ToString("D3");
         return dateTimeStr;
     }
 
     private string Nowdate() {
-        string dateTimeStr = System.DateTime.Now.Year  .ToString()  + "-"
-                           + System.DateTime.Now.Month .ToString()  + "-"
-                           + System.DateTime.Now.Day   .ToString()  + " "
-                           + System.DateTime.Now.Hour  .ToString()  + "-"
-                           + System.DateTime.Now.Minute.ToString()  + "-"
-                           + System.DateTime.Now.Second.ToString();
+        string dateTimeStr = System.DateTime.Now.Year.ToString("D4") + "／"
+                           + System.DateTime.Now.Month.ToString("D2") + "／"
+                           + System.DateTime.Now.Day.ToString("D2") + " "
+                           + System.DateTime.Now.Hour.ToString("D2") + "："
+                           + System.DateTime.Now.Minute.ToString("D2") + "："
+                           + System.DateTime.Now.Second.ToString("D2");
         return dateTimeStr;
     }
 
     private float NowTimeNum() {
-        float dateTimeFlo = System.DateTime.Now.Hour        * 3600
-                          + System.DateTime.Now.Minute      * 60
+        float dateTimeFlo = System.DateTime.Now.Hour * 3600
+                          + System.DateTime.Now.Minute * 60
                           + System.DateTime.Now.Second
                           + System.DateTime.Now.Millisecond * 0.001f;
         return dateTimeFlo;
